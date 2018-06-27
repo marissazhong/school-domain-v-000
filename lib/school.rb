a8749eb63a60b676f2de3e34370605c90bfc4518
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 19:21:12 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -27,5 +27,9 @@ end
 school = School.new("My school")
 school.add_student("Zach Morris",9)
 school.add_student("AC Slater", 9)
+school.add_student("Homer Simpson", 9)
+school.add_student("Jeff Baird", 10)
+school.add_student("Avi Flombaum", 10)
+school.add_student("Blake Johnson", 7)
 school.roster
 school.grade(9)
