dc17155a1349696265aab83daeb86800cce4ce7c
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 19:11:16 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+  end
+
+  roster = {}
+
+  def add_student(name, grade)
+    if roster.has_key? grade
+      roster[grade].push(name)
+    else
+      roster[grade] = [name]
+    end
+  end
+
+  def roster
+    puts roster
+  end
+
+end
