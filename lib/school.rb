8aea9eb27c7d3b506cf482c4636152d1bb7f8d5f
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 19:44:18 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,7 +5,6 @@ class School
     @roster = {}
   end
 
-
   def add_student(name, grade)
     if @roster.has_key? grade
       @roster[grade].push(name)
@@ -32,6 +31,7 @@ class School
 end
 
 school = School.new("My school")
+school.roster
 school.roster.length
 # school.add_student("Zach Morris",9)
 # school.add_student("AC Slater", 9)
